b7342a002059f2e9a3826953f2c124c296bc7ef3
v-lizy81 <v-lizy81.gd@chinatelecom.cn>
2023-06-29 00:25:31 +0800

    增加功能：支持声明式二进制协议

--- src/main/java/org/jetlinks/protocol/official/binary2/BinaryMessageCodec.java
+++ src/main/java/org/jetlinks/protocol/official/binary2/BinaryMessageCodec.java
@@ -1,4 +1,15 @@
 package org.jetlinks.protocol.official.binary2;
 
+import io.netty.buffer.ByteBuf;
+import org.jetlinks.core.message.DeviceMessage;
+
+/**
+ * 二进制消息的编解码
+ */
 public interface BinaryMessageCodec {
+
+    <T extends DeviceMessage> T  decode(ByteBuf buf);
+
+    ByteBuf encode(DeviceMessage message);
+
 }
