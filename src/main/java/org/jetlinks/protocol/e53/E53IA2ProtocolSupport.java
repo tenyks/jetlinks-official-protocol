97cf7f16b3908d6512d3655048091c6ff2515fae
tenyks <tenyks@163.com>
2024-03-20 23:33:33 +0800

    增加功能：E53 IA2协议

---
+++ src/main/java/org/jetlinks/protocol/e53/E53IA2ProtocolSupport.java
@@ -0,0 +1,12 @@
+package org.jetlinks.protocol.e53;
+
+/**
+ * E53版IA2协议
+ * @author v-lizy81
+ * @date 2024/3/20 21:42
+ */
+public class E53IA2ProtocolSupport {
+
+    public static final String NAME_OF_IA2 = "E53_IA2_V1.0.0";
+
+}
